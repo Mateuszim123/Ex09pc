e4f0ad62d511a4e0abcb393f75d7a9c1ab5c09b2
Mateus Cosma <mc_cosma@hotmail.com>
2023-11-29 01:31:02 -0300

    Adicionar arquivos de projeto.

---
+++ Ex09pc/EntradaDeDados.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio09
+{
+    public class EntradaDados
+    {
+        public int LeInteiro(string mensagem) //Entrada de dados com consistência - executa até ser digitado um inteiro. Mensagem de entrada inválida
+        {
+            string aux;
+            int n = 0;
+            bool ok = false;
+            do
+            {
+                Console.Write(mensagem);
+                aux = Console.ReadLine();
+                if (aux == string.Empty)
+                {
+                    Console.WriteLine("Favor informar os dados!");
+                }
+                else if (int.TryParse(aux, out n))
+                {
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine("Número inválido! Tente Novamente!");
+                }
+            } while (ok == false);
+            return n;
+        }
+
+        public double LeDouble(string mensagem) //Entrada de dados com consistência - executa até ser digitado um inteiro. Mensagem de entrada inválida
+        {
+            string aux;
+            double n = 0.0;
+            bool ok = false;
+            do
+            {
+                Console.Write(mensagem);
+                aux = Console.ReadLine();
+                if (aux == string.Empty)
+                {
+                    Console.WriteLine("Favor informar os dados!");
+                }
+                else if (double.TryParse(aux, out n))
+                {
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine("Número inválido! Tente Novamente!");
+                }
+            } while (ok == false);
+            return n;
+        }
+        public char LeChar(string mensagem) //Entrada de dados com consistência - executa até ser digitado um inteiro. Mensagem de entrada inválida
+        {
+            string aux;
+            char c = ' ';
+            bool ok = false;
+            do
+            {
+                Console.Write(mensagem);
+                aux = Console.ReadLine();
+                if (aux == string.Empty)
+                {
+                    Console.WriteLine("Favor informar os dados!");
+                }
+                else if (char.TryParse(aux, out c))
+                {
+                    ok = true;
+                }
+                else
+                {
+                    Console.WriteLine("Caracter inválido! Tente Novamente!");
+                }
+            } while (ok == false);
+            return c;
+        }
+        public string LeString(string mensagem) //Entrada de dados com consistência - executa até ser digitado um inteiro. Mensagem de entrada inválida
+        {
+            string aux;
+            bool ok = false;
+            do
+            {
+                Console.Write(mensagem);
+                aux = Console.ReadLine();
+                if (aux == string.Empty)
+                {
+                    Console.WriteLine("Favor informar os dados!");
+                }
+                else
+                {
+                    ok = true;
+                }
+
+            } while (ok == false);
+            return aux;
+        }
+    }
+}
---
+++ Ex09pc/ItemBiblioteca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio09
+{
+    public class ItemBiblioteca
+    {
+        int Identificacao;
+        string Titulo;
+        string Situacao;
+
+        public ItemBiblioteca(int identificacao, string titulo, string situacao)
+        {
+            this.Identificacao = identificacao;
+            this.Titulo = titulo;
+            this. Situacao = situacao;
+        }
+
+        public override string ToString()
+        {
+            return $"IDENTIFICAÇÃO: {Identificacao}\nTITULO: {Titulo}\nSITUAÇÃO: {Situacao}";
+        }
+    }
+}
